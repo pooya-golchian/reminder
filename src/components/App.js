82f0c79b83fb4af21a60a7f056b9472f13ffaea9
pooya golchian <pooya.golchian@gmail.com>
2018-04-07 15:58:20 +0430

    update all files without styles

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder, deleteReminder, clearReminders } from './../actions';
+import moment from 'moment';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      dueDate: ''
+    }
+  }
+
+  addReminder() {
+    console.log('this.state.dueDate', this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+
+  deleteReminder(id) {
+    this.props.deleteReminder(id);
+  }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="">
+                <div className="">
+                  <div>{reminder.text}</div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                </div>
+                <div
+                  className=""
+                  onClick={() => this.deleteReminder(reminder.id)}
+                >
+                  &#x2715;
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className="">
+        <div className="">
+          Reminder Pro
+        </div>
+        <div className="">
+          <div className="">
+            <input
+              className=""
+              placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
+            />
+            <input
+              className=""
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
+            />
+          </div>
+          <button
+            type="button"
+            className=""
+            onClick={() => this.addReminder()}
+          >
+            Add Reminder
+          </button>
+        </div>
+        { this.renderReminders() }
+        <div
+          className=""
+          onClick={() => this.props.clearReminders()}
+        >
+          Clear Reminders
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,40 @@
-import { ADD_REMINDER} from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
-    return {
-        text:action.text,
-        id: Math.random(),
-    }
+  let { text, dueDate } = action;
+  return {
+    id: Math.random(),
+    text,
+    dueDate
+  }
 }
 
-const reminders = (state=[], action) => {
-     let reminders = null;
-     switch (action.type) {
-         case ADD_REMINDER:
-            reminders = [...state, reminder(action)];
-            console.log('reminder as state' , reminders);
-            return reminders;
-        default:
-            return state;
-     }
+const removeById = (state = [], id) => {
+  const reminders = state.filter(reminder => reminder.id !== id);
+  console.log('new reduced reminders', reminders);
+  return reminders;
 }
 
+const reminders = (state = [], action) => {
+  let reminders = null;
+  state = read_cookie('reminders');
+  switch(action.type) {
+    case ADD_REMINDER:
+      reminders = [...state, reminder(action)];
+      bake_cookie('reminders', reminders);
+      return reminders;
+    case DELETE_REMINDER:
+      reminders = removeById(state, action.id);
+      bake_cookie('reminders', reminders);
+      return reminders;
+    case CLEAR_REMINDERS:
+      reminders = [];
+      bake_cookie('reminders', reminders);
+      return reminders;
+    default:
+      return state;
+  }
+}
 
 export default reminders;
