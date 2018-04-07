19b795b499b7c997b20a3e442132e9db96964c1a
pooya golchian <pooya.golchian@gmail.com>
2018-04-07 14:54:33 +0430

    update all js files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { addReminder } from './actions';
+
+
 
 class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+  }
+
+  addReminder() {
+    // console.log('this.state', this.state);
+    // console.log('this', this);
+    this.props.addReminder(this.state.text);
+  }
+
+
+
   render() {
+    console.log('this.props', this.props);
     return (
       <div className="App">
           <div className="title">Rminder</div>
@@ -10,8 +32,12 @@ class App extends Component {
                 <div className="">
                    <input className=""
                    placeholder="I have to ..."
+                   onChange={ event => this.setState({ text: event.target.value }) }
                    />
-                   <button className="" type="button">Add Rminder</button>
+                   <button
+                   className="" type="button"
+                   onClick={()=>this.addReminder()}
+                   >Add Rminder</button>
                 </div>
             </div>
       </div>
@@ -19,4 +45,19 @@ class App extends Component {
   }
 }
 
-export default App;
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({addReminder}, dispatch);
+//   }
+
+
+function mapStateToprops(state) {
+  // console.log('state',state);
+  return {
+    reminders: state
+  }
+}
+
+
+
+
+export default connect(null, {addReminder})(App);
